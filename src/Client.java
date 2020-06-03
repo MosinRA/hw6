857daa64ea8e2f2c1a56699f0cff2dfb0840545c
Roman Mosin <mosinra90@yandex.ru>
2020-06-03 21:17:11 +0500

    HW6

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,34 @@
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+class Client {
+    private static int POTR = 3333;
+    private static String IP = "localhost";
+
+    public static void main(String[] args) throws IOException {
+
+        Socket server = null;
+
+        try {
+            server = new Socket(IP, POTR);
+        } catch (UnknownHostException e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+
+        PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+        String msg;
+
+        while ((msg = stdIn.readLine()) != null) {
+            out.println(msg);
+            System.out.println(in.readLine());
+        }
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,78 @@
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+class Server {
+    private static int PORT = 3333;
+
+    public static void main(String[] args) throws IOException {
+
+        class ClientConn implements Runnable {
+            private Socket client;
+
+            ClientConn(Socket client) {
+                this.client = client;
+            }
+
+            public void run() {
+                BufferedReader in = null;
+                PrintWriter out = null;
+
+                try {
+                    in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    out = new PrintWriter(client.getOutputStream(), true);
+                    BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+                    String msg;
+
+                    while ((msg = stdIn.readLine()) != null) {
+                        out.println(msg);
+                        System.out.println(in.readLine());
+                    }
+                } catch (IOException e) {
+                    System.err.println(e);
+                    return;
+                }
+
+                String msg;
+                try {
+                    while ((msg = in.readLine()) != null) {
+                        System.out.println("Клиент: " + msg);
+
+                    }
+                } catch (IOException e) {
+                    System.err.println(e);
+                }
+            }
+        }
+
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Сервер запущен!");
+
+        } catch (IOException e) {
+            System.err.println("Нет ответа от порта: " + PORT);
+            System.err.println(e);
+            System.exit(1);
+        }
+
+        Socket client = null;
+        while (true) {
+            try {
+                client = server.accept();
+                System.out.println("Клиент подключился!");
+            } catch (IOException e) {
+                System.err.println("Ошибка");
+                System.err.println(e);
+                System.exit(1);
+            }
+            Thread t = new Thread(new ClientConn(client));
+            t.start();
+        }
+    }
 }
